9ec1f7d5a39ea5ffeec70d79bd2b2bc8f5824176
Bourguignon Roman <roman.bourguignon@gmail.com>
2024-05-11 14:38:48 +0200

    Page contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,21 +7,21 @@ import { Card } from "../components/card";
 const socials = [
 	{
 		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
+		href: "https://twitter.com/Oxamoxa2",
 		label: "Twitter",
-		handle: "@chronark_",
+		handle: "@Oxamoxa2",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:roman.bourguignon@gmail.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "roman.bourguignon@gmail.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/Oxamoxa/portfolio/",
 		label: "Github",
-		handle: "chronark",
+		handle: "Oxamoxa",
 	},
 ];
 
