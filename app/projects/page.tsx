1cc90f9f69507f433dcf515d709f28a28e5bf9c2
Bourguignon Roman <roman.bourguignon@gmail.com>
2024-05-16 01:34:15 +0200

    Retour

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -36,7 +36,7 @@ export default async function ProjectsPage() {
             Projects
           </h2>
           <p className="mt-4 text-zinc-400">
-            Ici sont regroupés tous mes projets, qu'ils soient réalisés seul ou bien en groupe, durant mes années de BTS SIO.
+            Some of the projects are from work and some are on my own time.
           </p>
         </div>
         <div className="w-full h-px bg-zinc-800" />
