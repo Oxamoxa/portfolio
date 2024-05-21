2e41ce524f0d50aeea308d6dfb91a45dfa80cdc1
Bourguignon Roman <roman.bourguignon@gmail.com>
2024-05-21 17:42:54 +0200

    Changement projet

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -10,9 +10,9 @@ import { Eye } from "lucide-react";
 export const revalidate = 60;
 export default async function ProjectsPage() {
 
-  const featured = allProjects.find((project) => project.slug === "unkey")!;
-  const top2 = allProjects.find((project) => project.slug === "planetfall")!;
-  const top3 = allProjects.find((project) => project.slug === "highstorm")!;
+  const featured = allProjects.find((project) => project.slug === "portfolio")!;
+  const top2 = allProjects.find((project) => project.slug === "GVH")!;
+  const top3 = allProjects.find((project) => project.slug === "Infoclimat")!;
   const sorted = allProjects
     .filter((p) => p.published)
     .filter(
