1601a38f330e010462d78adc50ec4d0d51fae760
Bourguignon Roman <roman.bourguignon@gmail.com>
2025-02-06 16:46:56 +0100

    Formulaire contact avec Captcha

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -17,27 +17,27 @@ export default async function handler(req, res) {
 
     const { name, email, message, recaptchaToken } = req.body;
 
-    // try {
-    //     const recaptchaResponse = await fetch("https://www.google.com/recaptcha/api/siteverify", {
-    //         method: "POST",
-    //         headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    //         body: new URLSearchParams({
-    //             secret: process.env.RECAPTCHA_SECRET_KEY,
-    //             response: recaptchaToken,
-    //         }),
-    //     });
+    try {
+        const recaptchaResponse = await fetch("https://www.google.com/recaptcha/api/siteverify", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: new URLSearchParams({
+                secret: process.env.RECAPTCHA_SECRET_KEY,
+                response: recaptchaToken,
+            }),
+        });
 
-    //     const recaptchaData = await recaptchaResponse.json();
-    //     console.log("Réponse reCAPTCHA:", recaptchaData);
+        const recaptchaData = await recaptchaResponse.json();
+        console.log("Réponse reCAPTCHA:", recaptchaData);
 
-    //     if (!recaptchaData.success) {
-    //         console.log("Échec de la vérification reCAPTCHA");
-    //         return res.status(400).json({ error: "Échec de la vérification reCAPTCHA", details: recaptchaData });
-    //     }
-    // } catch (error) {
-    //     console.error("Erreur lors de la requête reCAPTCHA:", error);
-    //     return res.status(500).json({ error: "Erreur lors de la requête reCAPTCHA", details: error.message });
-    // }
+        if (!recaptchaData.success) {
+            console.log("Échec de la vérification reCAPTCHA");
+            return res.status(400).json({ error: "Échec de la vérification reCAPTCHA", details: recaptchaData });
+        }
+    } catch (error) {
+        console.error("Erreur lors de la requête reCAPTCHA:", error);
+        return res.status(500).json({ error: "Erreur lors de la requête reCAPTCHA", details: error.message });
+    }
 
     // Configuration de Nodemailer
     const transporter = nodemailer.createTransport({
